7324656f0d90dec0f98a77b10d83dd7111254c23
Lucas Lopes <lucaslopes@infoway-pi.com.br>
2022-06-04 20:42:21 -0300

    correção de código morse

--- producer.py
+++ producer.py
@@ -4,21 +4,31 @@ from json  import dumps
 from kafka import KafkaProducer
 
 MORSE_CODE = {
-    1: '.----', 2: '..---', 3: '...--',
-    4: '....-', 5: '.....', 6: '-....',
-    7: '--...', 8: '---..', 9: '----.',
-    0: '-----'
+    '1': '.----', '2': '..---', '3': '...--',
+    '4': '....-', '5': '.....', '6': '-....',
+    '7': '--...', '8': '---..', '9': '----.',
+    '0': '-----'
 }
+
+def morse_code(n):
+    if n < 10:
+        return MORSE_CODE[str(n)]
+    else:
+        morse = ""
+        for np in str(n):
+            morse += MORSE_CODE[str(np)] + " "
+        
+        return morse[:-1]
 
 producer = KafkaProducer(
     bootstrap_servers = ['localhost:9092'],
     value_serializer  = lambda x: dumps(x).encode('utf-8')
 )
 
-for n in range(100):
+for n in range(10000):
     data = {
         'Número': n,
-        'Morse': MORSE_CODE[n]
+        'Morse': morse_code(n)
     }
     producer.send('test-topic', value = data)
-    sleep(5)
+    # sleep(1)
